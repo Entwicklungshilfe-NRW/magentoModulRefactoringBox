61dd797e679176ee5e9426c9f29cb27ab2bbf082
Andreas Mautz <am@webvisum.de>
2015-08-14 11:00:54 +0200

    [INFRASTRUCTURE] small changes for php version and unittesting

--- puppet/files/exec-once/90-db-import.sh
+++ puppet/files/exec-once/90-db-import.sh
@@ -1,29 +1,22 @@
 #!/bin/sh
 
-echo "Looking for database dump in systemstorage/live/database/..."
-DUMP=`ls -1t /vagrant/systemstorage/database/*.sql 2> /dev/null | head -n1`
+echo "Importing SQL dump from $DUMP. This will take a while..."
+echo "You can ignore warnings about using a password on the command line interface." >&2
 
-if [ "$DUMP" != "" ]; then
+DBH="localhost";
+DBU="root"
+DBP="root"
+DBN="magento"
+DUMP="magentoTestWithSampleData.sql"
 
-	echo "Importing SQL dump from $DUMP. This will take a while..."
-	echo "You can ignore warnings about using a password on the command line interface." >&2
+echo "Import database magento"
+mysql -h ${DBH} -u${DBU} -p${DBP} -e "create database if not exists ${DBN} ";
+mysql -h ${DBH} -u${DBU} -p${DBP} ${DBN} < ${DUMP}
 
-	DBH="localhost";
-	DBU="root"
-	DBP="root"
-	DBN="magento"
+DBN="magento_unit_tests"
+DUMP="magentoUnitTests.sql"
+echo "Import database magento_unit_tests"
+mysql -h ${DBH} -u${DBU} -p${DBP} -e "create database if not exists ${DBN} ";
+mysql -h ${DBH} -u${DBU} -p${DBP} ${DBN} < ${DUMP}
 
-	echo "1) Drop database"
-	(echo "SET foreign_key_checks = 0;";
-	 (mysqldump -h ${DBH} -u${DBU} -p${DBP} --add-drop-table --no-data ${DBN} |
-		 grep ^DROP);
-	 echo "SET foreign_key_checks = 1;") | mysql -h ${DBH} -u${DBU} -p${DBP} ${DBN}
-
-	echo "2) Import database"
-	mysql -h ${DBH} -u${DBU} -p${DBP} -e "create database if not exists ${DBN} ";
-	mysql -h ${DBH} -u${DBU} -p${DBP} ${DBN} < ${DUMP}
-
-	echo "Done importing SQL dump."
-else
-	echo "No database dump found."
-fi
+echo "Done importing SQL dump."
